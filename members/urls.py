b84bcecefa381ded1a1cde30bad83f55a48744f2
Execise <vajarinkarapurva>
2023-02-20 17:30:00 +0530

    testing

--- members/urls.py
+++ members/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
+    
+    #path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord'),
+
 ]
+
--- members/views.py
+++ members/views.py
@@ -1,13 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
-#from django.template import loader
-
-def members(request):
-    a= "myfirst.html"
-    return render (request,'myfirst.html',{'a':a})
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Member
+# def members(request):
+#     a= "myfirst.html"
+#     return render (request,'myfirst.html',{'a':a})
     #template = loader.get_template('myfirst.html')
     #return HttpResponse(template.render())
     #return HttpResponse("Hello world!")
+
+def members(request):
+  mymembers = Member.objects.all().values()
+#   template = loader.get_template('myfirst.html')
+#   context = {
+#     'mymembers': mymembers,
+#   }
+  #return HttpResponse(template.render(context, request))
+  return render(request,'add.html', {'mymembers': mymembers})
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  x = request.POST['first']
+  y = request.POST['last']
+  member = Member(firstname=x, lastname=y)
+  member.save()
+  return HttpResponseRedirect(reverse('members'))  
+  
+def addrecord(request):
+      if request.method == 'POST':
+        # firstname = 'priya'
+        # lastname = 'patil'
+        obj = members.objects.create(firstname = 'priya',
+        lastname = 'patil')
+        obj.save()
+  
+
+  
